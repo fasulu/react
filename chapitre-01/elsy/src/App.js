d37e9aa1127705810054b5eafafaeb08df846f61
unknown <rfasulu@hotmail.com>
2021-04-07 15:40:50 +0200

    Elsy Step1, Step2

---
+++ chapitre-01/elsy/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './App.css';
+
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepMax = 50000;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <div className="row">
+
+          <h1 className="text-primary">Hello !</h1>
+          <p> Heart: {heartMin}</p>
+          <p> Temperature: {tempMin}</p>
+          <p> Steps: {stepsMin}</p>
+
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
