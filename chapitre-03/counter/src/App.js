20e27f3cb7f807cf9e2fc3b4c1f2e554d6c6e78c
unknown <rfasulu@hotmail.com>
2021-04-12 22:17:44 +0200

    Counter Updated with 2 Counters and Buttons Equal Increment/Decrement not working"

--- chapitre-03/counter/src/App.js
+++ chapitre-03/counter/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Counter from './component/Counter.js';
-import './styles/global.css';
+// import './styles/global.css';
 
 class App extends React.Component {
 
@@ -9,18 +9,23 @@ class App extends React.Component {
     super()
 
     this.state = {                                          // initialize counter with zero
-      count: 0
+      count1: 0,
+      count2: 0
     }
 
-    this.decrementCount = this.decrementCount.bind(this)    // bind current decrementCount function value with decrementCount
-    this.incrementCount = this.incrementCount.bind(this)    // bind current incrementCount function value with incrementCount
+
+    this.decrementCount1 = this.decrementCount1.bind(this)    // bind current decrementCount function value with decrementCount
+    this.incrementCount1 = this.incrementCount1.bind(this)    // bind current incrementCount function value with incrementCount
+
+    this.decrementCount2 = this.decrementCount2.bind(this)    // bind current decrementCount function value with decrementCount
+    this.incrementCount2 = this.incrementCount2.bind(this)    // bind current incrementCount function value with incrementCount
   }
 
-  decrementCount() {                                        // create function to decrement current counter value with one
-    const { count } = this.state                            // move current counter value to decrementCount constant
+  decrementCount1() {                                        // create function to decrement current counter value with one
+    const { count1 } = this.state                            // move current counter value to decrementCount constant
 
     this.setState({                                         // using setState to change 
-      count: count - 1                                      // decrement counter current value by 1
+      count1: count1 - 1                                      // decrement counter current value by 1
     })
   }
 
@@ -32,11 +37,11 @@ class App extends React.Component {
   //   })
   // }
 
-  incrementCount() {
-    const { count } = this.state
+  incrementCount1() {
+    const { count1 } = this.state
 
     this.setState({
-      count: count + 1
+      count1: count1 + 1
     })
 
     // this.setState({
@@ -44,14 +49,46 @@ class App extends React.Component {
     // })
   }
 
+  decrementCount2() {                                        // create function to decrement current counter value with one
+    const { count2 } = this.state                            // move current counter value to decrementCount constant
+
+    this.setState({                                         // using setState to change 
+      count2: count2 - 1                                      // decrement counter current value by 1
+    })
+  }
+
+  incrementCount2() {
+    const { count2 } = this.state
+
+    this.setState({
+      count2: count2 + 1
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        <h1>Counter</h1>
-        
-        <Counter count={this.state.count} decrementFunction={this.decrementCount} incrementFunction={this.incrementCount} />
 
-      </div>
+      <div>
+
+        <div>
+          <h1>Counter A</h1>
+
+          <Counter count1={this.state.count1} decrementFunction1={this.decrementCount1} incrementFunction1={this.incrementCount1} />
+
+
+        </div>
+
+
+        <div>
+
+          <h1>Counter B</h1>
+
+          <Counter count2={this.state.count2} decrementFunction2={this.decrementCount2} incrementFunction2={this.incrementCount2} />
+
+        </div>
+
+      </div >
     )
   }
 }
@@ -108,7 +145,7 @@ export default App
 //         <h1>Counter</h1>
 
 //         <h2>{this.state.count}</h2>     {/* show current counter value */}
-        
+
 //         <button className="redbg" onClick={this.decrementCount}>-</button>      {/* pass each button click to decrementCount function  */}
 
 //         <button className="greenbg" onClick={this.incrementCount}>+</button>    {/* pass each button click to incrementCount function */}
--- chapitre-03/counter/src/component/Counter.js
+++ chapitre-03/counter/src/component/Counter.js
@@ -7,39 +7,46 @@ class Counter extends React.Component {
 
 
     render() {
-
-        let counterVal = this.props.count;
-
-        if (counterVal > 10) {
+        
+        let counter1Val = this.props.count1;
+        
+        let counter2Val = this.props.count2;
+        
+        if (counter1Val > 5) {
             alert("Value must not be more than 100")
             return ("Exiting with error, refresh to continue");
-        } else if (counterVal < 0) {
+        } else if (counter1Val < 0) {
             alert("Value must not be less than 0")
             return ("Exiting with error, refresh to continue");
         }
 
+        if (counter2Val > 5) {
+            alert("Value must not be more than 100")
+            return ("Exiting with error, refresh to continue");
+        } else if (counter2Val < 0) {
+            alert("Value must not be less than 0")
+            return ("Exiting with error, refresh to continue");
+        }
 
         return (
             <div>
 
-                <h2>{this.props.count}</h2>
 
                 <div>
 
-                    <button style={{ color: "black", backgroundColor: "red", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.decrementFunction}>-</button>
+                    <h2>{counter1Val}</h2><h2>{counter2Val}</h2>
 
-                    <button style={{ color: "yellow", backgroundColor: "green", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.incrementFunction}>+</button>
+                    <button style={{ color: "black", backgroundColor: "red", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.decrementFunction1}>-</button>
+
+                    <button style={{ color: "yellow", backgroundColor: "green", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.incrementFunction1}>+</button>
+
+                    
+
+                    <button style={{ color: "black", backgroundColor: "red", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.decrementFunction2}>-</button>
+
+                    <button style={{ color: "yellow", backgroundColor: "green", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.incrementFunction2}>+</button>
 
                 </div>
-
-                <div>
-
-                    <button style={{ color: "black", backgroundColor: "red", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.decrementFunction}>-</button>
-
-                    <button style={{ color: "yellow", backgroundColor: "green", fontSize: 25, width: 100, borderRadius: 10, borderColor: "white" }} onClick={this.props.incrementFunction}>+</button>
-
-                </div>
-
 
             </div>
 
