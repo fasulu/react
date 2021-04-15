b5691770d939ff9df53c66c2e4821f83100a57be
unknown <rfasulu@hotmail.com>
2021-04-15 17:27:15 +0200

    Form validation progressed

--- chapitre-04/form/src/App.js
+++ chapitre-04/form/src/App.js
@@ -17,24 +17,31 @@ class App extends Component {
     this.handleEmail = this.handleEmail.bind(this)
     this.checkboxData = this.checkboxData.bind(this)
     this.submitData = this.submitData.bind(this)
+    this.handlePassword = this.handlePassword.bind(this)
+
   }
 
   handleEmail(event) {
 
     let email = event.target.value
-    console.log(event.target.value)
-    console.log("email from handleEmail", email)
 
+    // console.log(event.target.value)
+    // console.log("email from handleEmail", email)
+    
     this.setState({
       email: event.target.value
     })
+
+    const emailRegEx = /^[^\s@]+@[^\s@]+$/;   // to prevent multiple @ sign
+
+    console.log("this is valid or not", emailRegEx.test(email));
   }
 
   handlePassword(event) {
 
     let pwd = event.target.value
 
-    console.log("password text", pwd)
+    // console.log("password text", pwd)
 
     this.setState({
       password: event.target.value
@@ -54,8 +61,22 @@ class App extends Component {
 
   submitData(event) {
 
-    let email1 = this.state.email
-    console.log("email from submitdata", email1)
+    let email = this.state.email
+
+    console.log("email from submitdata", this.state.email)
+
+    const emailRegEx = /^[^\s@]+@[^\s@]+$/;   // to prevent multiple @ sign
+
+    if(emailRegEx.test(email)) {
+
+      console.log("this is valid or not", true,  emailRegEx.test(email));
+    
+    } else {
+      console.log(false)
+    }
+    
+
+
 
   }
 
@@ -63,36 +84,36 @@ class App extends Component {
 
     return (
 
-      <div className="container-sm bg-light">
+      <div>
 
-        <div className="row">
-
+        <div className="col-6 mb-3 offset-3">
           <div>
             <label for="inputText" class="form-label">Email </label> <br></br>
-            <input type="email" id="inputEmail" class="emailInput" onChange={this.handleEmail}></input>
-          </div>
-          </div>
-          <div className="row">
-          <div className="col">
-            <div>
-              <label for="inputPassword5" class="form-label">Password</label>
-              <input id="inputPassword" class="pwdInput " onChange={this.handlePassword}></input>  {/* type="password"  */}
-            </div>
+
+            <input type="email" id="inputEmail" class="emailInput" onChange={this.handleEmail} value={this.state.email}></input>
           </div>
         </div>
 
-        <div className="row p-2 ">
-          <div className="">
+
+        <div className="col-6 mb-3 offset-3">
+          <div>
+            <label for="inputPassword" class="form-label">Password</label><br></br>
+            <input id="inputPassword" class="pwdInput" onChange={this.handlePassword} value={this.state.password}></input>  {/* type="password"  */}
+
+
+          </div>
+
+          <div className="mb-3">
             <input className="mx-1 " id="checkbox" type="checkbox" onClick={this.checkboxData} ></input>
             <label className=""  >Remember Me</label>
           </div>
-        </div>
-        <div className="row p-2 ">
-          <div className="">
-            <button style={{ background: "lightGrey" }} onClick={this.submitData} email={this.state.email} password={this.state.password}>Submit</button>
+
+          <div className="row p-2 ">
+            <div className="">
+              <button style={{ background: "lightGrey" }} onClick={this.submitData} email={this.state.email} password={this.state.password}>Submit</button>
+            </div>
           </div>
         </div>
-
         <p>email: {this.state.email}</p>
         <p>checkbox: {this.state.checkbox}</p>
         <p>password: {this.state.password}</p>
