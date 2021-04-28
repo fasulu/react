fddeecabb5a551657aa7fba805e2ed19200885b1
fasulu <rfasulu@hotmail.com>
2021-04-28 23:00:48 +0200

    Countries ex1, 2, 4, bonus finished - sytle is pending

--- chapitre-05/countries/src/App.js
+++ chapitre-05/countries/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Button from './components/Button';
+import Card from './components/Card';
 
 class App extends React.Component {
 
@@ -16,96 +17,139 @@ class App extends React.Component {
       population: "",
       region: ""
     }
+    this.findCountryFlag = this.findCountryFlag.bind(this)
   }
 
+
   componentDidMount() {
-    
-    fetch('https://restcountries.eu/rest/v2/france')
-    .then(response => response.json())
-    .then( countryArray  => {
 
-      this.setState({
-        name: countryArray[0].name,
-        capital: countryArray[0].capital,
-        flag: countryArray[0].flag,
-        population: countryArray[0].population,
-        region: countryArray[0].region
-      })
+    //  https://restcountries.eu/rest/v2/name/france
+    fetch('https://restcountries.eu/rest/v2/name/france')
+      .then(response => response.json())
+      .then(countryArray => {
 
-      // console.log(this.state.name)
+        this.setState({
+          name: countryArray[0].name,
+          capital: countryArray[0].capital,
+          flag: countryArray[0].flag,
+          population: countryArray[0].population,
+          region: countryArray[0].region
+        })
+
+        // console.log(this.state.name)
       }
-    )
-    .catch(err => console.error("there is an error",  err));
+      )
+      .catch(err => console.error("there is an error", err));
   }
 
   fetchCountry(country) {
 
-    console.log("fetch country",country)
-    const url = 'https://restcountries.eu/rest/v2/' + this.state.name;
-    console.log("url is",url)
+    console.log("fetch country", country)
+    const url = 'https://restcountries.eu/rest/v2/name/' + country;
+    console.log("url is", url)
 
     fetch(url)
-    .then(response => response.json())
-    .then( countryArray  => {
+      .then(response => response.json())
+      .then(countryArray => {
 
-      this.setState({
-        name: countryArray[0].name,
-        capital: countryArray[0].capital,
-        flag: countryArray[0].flag,
-        population: countryArray[0].population,
-        region: countryArray[0].region
-      })
+        this.setState({
+          name: countryArray[0].name,
+          capital: countryArray[0].capital,
+          flag: countryArray[0].flag,
+          population: countryArray[0].population,
+          region: countryArray[0].region
+        })
 
-      // console.log(this.state.name)
+        // console.log(this.state.name)
       }
-    )
-    .catch(err => console.error("there is an error",  err));
+      )
+      .catch(err => console.error("there is an error", err));
   }
+
 
   onClickFrance = () => {
 
-    this.fetchCountry()
+    // console.log("im in france")
+    this.fetchCountry("france")
 
   }
 
   onClickBrazil = () => {
 
-    this.setState({
-      name: "brazil"
-    })
-    this.fetchCountry()
+    // console.log("im in brazil")
+    this.fetchCountry("brazil")
+
   }
 
   onClickCroatia = () => {
 
+    // console.log("im in croatisa")
+    this.fetchCountry("croatia")
+
+  }
+
+  onClickShowCard() {
+    // console.log(this.state.name)
+    this.fetchCountry(this.state.name)
+  }
+
+  findCountryFlag(event) {
+    // console.log(event.target.value)
     this.setState({
-      name: "croatia"
+      name: event.target.value
     })
-    this.fetchCountry()
+
   }
 
   render() {
 
     return (
       <div>
+        <div>
         <h6>{this.state.name}</h6>
         <h6>{this.state.capital}</h6>
         <h6>{this.state.flag}</h6>
         <h6>{this.state.population}</h6>
         <h6>{this.state.region}</h6>
+        </div>
 
 
         <div className="container-fluid">
           <div className="row">
-            <div className="col-lg-6">
-              <Button onClickFuction = {() => this.onClickFrance()} value={"france"}> France </Button>
-              <Button onClickFuction = {() => this.onClickBrazil()} value={"brazil"}> Brazil </Button>
-              <Button onClickFuction = {() => this.onClickCroatia()} value={"croatia"}> Croatia</Button>
+            <div className="col-lg-md">
+              <Button className="btn btn-primary"
+                onClick={() => this.onClickFrance()} name="France" />
+              <Button className="btn btn-primary"
+                onClick={() => this.onClickBrazil()} name="Brazil" />
+              <Button className="btn btn-primary"
+                onClick={() => this.onClickCroatia()} name="Croatia" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-md">
+
+              <input className="" 
+              onChange={this.findCountryFlag} 
+              type="text" 
+              placeholder="type country name" />
+              <Card className="btn btn-outline-success" 
+              type="button" 
+              onClick={() => this.onClickShowCard()} 
+              name="Show"/>
+            </div>
+            <div className="row">
+            <div className="col-lg-md my-5 align-left">
+              <img src={this.state.flag} style={{ width: 150, height: "auto" }} />
             </div>
 
+            </div>
+              
+
+
+
+
+
           </div>
-
-
         </div>
 
       </div>
--- chapitre-05/countries/src/components/Card.jsx
+++ chapitre-05/countries/src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
-class Button extends Component {
+class Card extends Component {
     render(props) {
 
         console.log(this.props)
 
         return (
-            <button className="this.props.buttonName "
+            <button className="this.props.buttonName"
             onClick={() => this.props.onClick()}  > 
                 {this.props.children} {this.props.name}
             </button>
         )
     }
 }
-export default Button;
+export default Card;
